d5ad78c4e4aa728c547cca4c3c53f9c8282bffd2
YZTangent <yuanzheng.tan.tyz@gmail.com>
2025-09-27 16:18:24 +0800

    breaking: remove onedriver and cursor
    Onedriver removed because nus onedrive needs admin approval to use
    onedriver
    Cursor removed as Claude and Gemini CLI makes Cursor redundant

--- home.nix
+++ home.nix
@@ -49,10 +49,6 @@
       nativeMessagingHosts = [ pkgs.firefoxpwa ];
     })
 
-    onedriver
-
-    code-cursor
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
