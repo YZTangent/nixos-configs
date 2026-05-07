94b88394c6a5b328e7a4523f79909d5be01f3a55
YZTangent <yuanzheng.tan.tyz@gmail.com>
2026-05-08 00:24:21 +0800

    feat: add fcitx5 with Chinese pinyin input

--- modules/services.nix
+++ modules/services.nix
@@ -5,6 +5,12 @@
 
   fonts.packages = with pkgs; [ noto-fonts-cjk-sans noto-fonts-cjk-serif ];
 
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [ kdePackages.fcitx5-chinese-addons fcitx5-gtk ];
+  };
+
   services.xserver.enable = true;
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
