29d11e6223b49bc97f4a4a9fad0165f0b390c683
YZTangent <yuanzheng.tan.tyz@gmail.com>
2026-07-02 19:59:41 +0800

    feat: add nix-darwin support and macbook configuration

--- flake.nix
+++ flake.nix
@@ -4,6 +4,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -25,7 +30,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, darwin, ... }@inputs: {
     nixosConfigurations = {
       legion = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -41,6 +46,14 @@
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
         modules = [ ./hosts/x395.nix ];
+      };
+    };
+
+    darwinConfigurations = {
+      macbook = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        specialArgs = { inherit inputs; };
+        modules = [ ./hosts/macbook.nix ];
       };
     };
   };
