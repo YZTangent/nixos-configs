b19d90e1928eeadba47a982ec59df31d4ac93757
YZTangent <yuanzheng.tan.tyz@gmail.com>
2026-07-03 23:50:47 +0800

    fix: strix halo configs

--- hosts/strix-halo.nix
+++ hosts/strix-halo.nix
@@ -2,9 +2,11 @@
 
 {
   networking.hostName = "strix-halo";
+  networking.hostId = "strxhalo";
 
   imports = [
     ./default.nix
     ../hardware/strix-hardware-configuration.nix
+    ../hardware/gpu/amd.nix
   ];
 }
